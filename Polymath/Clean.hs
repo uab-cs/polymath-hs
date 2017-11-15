71e31dc5457723859e8bfcd81df9bbc70eaa00fd
Stone Tickle <stone.tickle@gmail.com>
2017-11-14 22:02:47 -0600

    make compatible with base 4.6

--- Polymath/Clean.hs
+++ Polymath/Clean.hs
@@ -2,7 +2,7 @@ module Polymath.Clean
   ( toPoly
   ) where
 
-import Data.Either(fromRight)
+import Data.Either
 import Text.ParserCombinators.Parsec
 
 import Polymath.Clean.Cleaner
@@ -12,5 +12,4 @@ import Polymath.Clean.Preprocessor
 toPoly :: Monad m => String -> m [Integer]
 toPoly str = do
   let s = sanitize str
-  let mess = fromRight zeroDegree (parse polynomial "fail" s)
-  return $ cleanup mess
+  either (return $ cleanup mess) (return [])  zeroDegree (parse polynomial "" s)
