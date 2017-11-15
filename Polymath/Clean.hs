838aa787e308e0a1b83c8c9ca536df41c328afba
Stone Tickle <stone.tickle@gmail.com>
2017-11-14 22:06:28 -0600

    whoops

--- Polymath/Clean.hs
+++ Polymath/Clean.hs
@@ -2,7 +2,7 @@ module Polymath.Clean
   ( toPoly
   ) where
 
-import Data.Either
+import Data.Either(fromRight)
 import Text.ParserCombinators.Parsec
 
 import Polymath.Clean.Cleaner
@@ -12,4 +12,5 @@ import Polymath.Clean.Preprocessor
 toPoly :: Monad m => String -> m [Integer]
 toPoly str = do
   let s = sanitize str
-  either (return $ cleanup mess) (return [])  zeroDegree (parse polynomial "" s)
+  let mess = either (return $ cleanup mess) (return [])  zeroDegree (parse polynomial "fail" s)
+  either (return []) (return cleanup) mess
