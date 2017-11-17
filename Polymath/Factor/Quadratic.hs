5c2d847a4f4e1f2a4b594a2b4f6eb0540fd66c3f
Stone Tickle <stone.tickle@gmail.com>
2017-11-16 20:32:23 -0600

    not working

--- Polymath/Factor/Quadratic.hs
+++ Polymath/Factor/Quadratic.hs
@@ -4,7 +4,12 @@ module Polymath.Factor.Quadratic
 
 import Data.Ratio
 
-quadratic :: Integral a -> [a] -> [Ratio a]
-quadratic (a:b:c:d) = let disc = sqrt( b^2 - (4 * a * c) )
-                      in [disc % 1]
+quadratic :: (Fractional a, Integral a) => [a] -> [Ratio a]
+quadratic (a:b:c:_) = let a' = fromIntegral a
+                          b' = fromIntegral b
+                          c' = fromIntegral c
+                      in let disc = sqrt(b'^2 - (4 * a' * c'))
+                             div  = (/) (2 * a')
+                             nb   = negate b'
+                         in [div (nb + disc) % 1, div (nb - disc) % 1]
 quadratic _ = []
